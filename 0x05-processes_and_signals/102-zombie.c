21aaa9aced77e7621786b271d84fbdd563ed4136
Nadira3 <paitanun35@gmail.com>
2023-10-07 04:39:06 +0100

    zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * main - A C program to demonstrate Zombie Process.
+ * Child becomes Zombie as parent is sleeping 
+ * when child process exits.
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	/* Fork returns process id in parent process */
+	pid_t child_pid = fork();
+
+	/* Parent process */
+	if (child_pid > 0)
+		sleep(50);
+
+	/* Child process */
+	else
+		exit(0);
+
+	return 0;
+}
