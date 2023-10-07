3a79f8b305155a499891382f32030a1c597fe09c
Nadira3 <paitanun35@gmail.com>
2023-10-07 06:02:56 +0100

    correction

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,26 +1,51 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <wait.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
 /**
  * main - A C program to demonstrate Zombie Process.
- * Child becomes Zombie as parent is sleeping 
+ * Child becomes Zombie as parent is sleeping
  * when child process exits.
- * 
+ *
  * Return: 0
  */
-int main(void)
+int zombie(void)
 {
+	int status;
+	pid_t child_pid;
+
 	/* Fork returns process id in parent process */
-	pid_t child_pid = fork();
+	child_pid = fork();
 
 	/* Parent process */
 	if (child_pid > 0)
-		sleep(50);
+	{
+		printf("Zombie process created, PID: %ld\n", (long)child_pid);
+	}
 
 	/* Child process */
 	else
 		exit(0);
+	
+	return (0);
+}
 
-	return 0;
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+		zombie();
+	infinite_while();
 }
