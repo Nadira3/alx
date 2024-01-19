b8cda2328a8a96e92832ae89b23dc7af0e84e0cb
Nadira3 <paitanun35@gmail.com>
2024-01-19 13:40:33 +0100

    task 0 submission

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,16 @@
-...
+# Requirements:
+# 
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
+
+file { '/tmp/school':
+  ensure => '/tmp/school',
+  path => '/tmp/school',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
+}
