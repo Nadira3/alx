c9fc3586c1f6581fabde77af3f9152bb37fff819
Nadira3 <paitanun35@gmail.com>
2024-04-27 09:40:14 +0100

    more correction upload

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,13 @@
 # automating config edit
-# puppet_ssh_config.pp
 
-# Define the SSH client configuration file path
-$ssh_config_file = '/etc/ssh/ssh_config'
-
-# Ensure SSH client configuration is set to use the private key ~/.ssh/school
-file_line { 'Declare identity file':
-  path   => $ssh_config_file,
-  line   => '    IdentityFile ~/.ssh/school',
-  match  => '^#?\s*IdentityFile',
-  ensure => present,
+file_line { ' passwd auth':
+ensure => present,
+line   => ' PasswordAuthentication no',
+path   => '/etc/ssh/ssh_config',
 }
 
-# Ensure SSH client configuration is set to refuse authentication using a password
-file_line { 'Turn off passwd auth':
-  path   => $ssh_config_file,
-  line   => '    PasswordAuthentication no',
-  match  => '^#?\s*PasswordAuthentication',
-  ensure => present,
+file_line { 'IdentityFile':
+ensure => present,
+line   => '   IdentityFile ~/.ssh/school',
+path   => '/etc/ssh/ssh_config',
 }
-
