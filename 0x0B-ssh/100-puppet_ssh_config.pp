8210b6f4fe838694555ec0ef3696ee07c8977089
Nadira3 <paitanun35@gmail.com>
2024-04-27 09:25:00 +0100

    task file fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,10 @@
 # Define SSH client configuration file content
-$file_content = @(CONTENT)
-Host your_server
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-CONTENT
-
-# Ensure SSH client configuration file exists and has the desired content
 file { '/home/u0_a171/.ssh/config':
   ensure  => present,
-  content => $file_content,
+  content => "\
+Host <server_hostname_or_IP>
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
   owner   => 'u0_a171',
 }
-
